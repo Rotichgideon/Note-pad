2675098d760a45cfe4a5c227b8d2da87a5d8ece0
gideon rotich <gideonrotich2001@gmail.com>
2024-05-09 00:20:15 +0300

    done

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Notes</li>
+      <li>Settings</li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
